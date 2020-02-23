701dcb9a18ff71f5b6dd839da36819407fe4c1db
Vineet-14 <56405984+Vineet-14@users.noreply.github.com>
2020-02-24 02:09:46 +0530

    head_only

--- snake.cpp
+++ snake.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
-int width=30;
-int height=15;
+#include<stdlib.h>
+#include<conio.h>
 using namespace std;
+int width=40;
+int height=20;
+bool gameover;
+int x,y,fx,fy;
+enum eDirection{STOP=0,RIGHT,LEFT,UP,DOWN };
+eDirection dir;
+void start(){
+	gameover=true;
+	dir=STOP;
+	x=width/2;
+	y=height/2;
+	fx=rand() %width;
+	fy=rand() %height;
+}
 void block(){
 	int i,j;
+	system("cls");
 	for (i=0;i<width+1;i++)
 		cout<<"#";
 	cout<<endl;
@@ -11,6 +26,10 @@ void block(){
 		for (j=0;j<width;j++){
 			if (j==0)
 				cout<<"#";
+			else if (i==y && j==x)
+				cout<<"O";
+			else if (i==fy && j==fx)
+				cout<<"F";
 			else
 				cout<<" ";
 			if (j==width-1)
@@ -22,7 +41,50 @@ void block(){
 		cout<<"#";
 	cout<<endl;
 }
+void input(){
+	char p;
+	if (_kbhit()){
+//		scanf ("%c",&p);
+		p=_getch();
+		switch(p){
+			case 'a': dir=LEFT;
+					break;
+			case 'd': dir=RIGHT;
+					break;
+			case 'w': dir=UP;
+					break;
+			case 's': dir=DOWN;
+					break;
+			default: gameover=false;
+		}
+	}
+}
+void logic(){
+	switch(dir){
+		case LEFT: x--;
+				break;
+		case RIGHT: x++;
+				break;
+		case UP: y--;
+				break;
+		case DOWN: y++;
+				break;
+	}
+	if (x==width-1 || x==0 || y==height-1 || y==0)
+		gameover=false;
+	if (x==fx && y==fy){
+		fx=rand()%width;
+		fy=rand()%height;
+	}
+}
 int main (){
-	block();
+	start();
+	while(gameover){
+		block();
+		input();
+		logic();
+//		block();
+		
+	}
 	return 0;
 }
